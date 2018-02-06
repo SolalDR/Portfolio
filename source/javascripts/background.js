ef02ffcd23b0169922d9da18a2788cfa32032bed
SolalDR <solal.dussout-revel@hotmail.fr>
2018-02-06 15:20:48 +0100

    Make background scrollable

--- source/javascripts/background.js
+++ source/javascripts/background.js
@@ -15,14 +15,14 @@ class Background {
 		this.canvas = document.querySelector("#bg-canvas");
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
-		this.mouseCoord = [-1000, -1000];
+		// this.mouseCoord = [-1000, -1000];
 
 		this.cursor = document.getElementById("cursor");
 		// this.cursor = {
 		// 	el: document.getElementById("cursor"),
 		// 	position: [window.innerWidth/2, window.innerHeight/2]
 		// }
-
+		this.scrollPercent = 0;
 		this.cursorPosition = [window.innerWidth/2, window.innerHeight/2]; 
 		var canvas = this.canvas;
 		// Initialize the GL context
@@ -38,6 +38,8 @@ class Background {
 
 		this.cursorPosition = { x: 0, y: 0 }
 		this.cursorExpectedPosition = { x: 0, y: 0 }
+
+		this.cursorPositionScaled = [0, 0]
 		this.wave = { coords: [this.cursorPosition.x, this.cursorPosition.y], radius: 0, speed: 20, strength: 0, needsUpdate: false, start: 0 };
 
 		this.updateDrawCommand(0);
@@ -48,25 +50,29 @@ class Background {
 
 
 	compute(){
-		let nW = Math.floor(window.innerWidth / config.bg.precision);
-		let nH = Math.floor(window.innerHeight / config.bg.precision);
+		let nW = Math.floor(window.innerWidth / config.bg.precision) + 1;
+		let nH = Math.floor(window.innerHeight / config.bg.precision) + 1;
+
+		let calcW = nW * config.bg.precision; 
+		let calcH = nH * config.bg.precision; 
+
 
 		this.points = [];
 		
-		var wParticule = 2/window.innerWidth*2
-		var hParticule = 2/window.innerHeight*2
+		var wParticule = 1.5/window.innerWidth*2
+		var hParticule = 1.5/window.innerHeight*2
 		var geometry =  new Polygon(3, wParticule, hParticule).points;
 		
-		var offsetX = 1 - (nW - 1) * config.bg.precision/window.innerWidth
-		var offsetY = 1 - (nH - 1) * config.bg.precision/window.innerHeight
+		var offsetX = 1 - (nW - 1) * config.bg.precision/calcW
+		var offsetY = 1 - (nH - 1) * config.bg.precision/calcH
 
 		for(var i=0; i<nW; i++) {
 			for(var j=0; j<nH; j++) {
 				this.points.push(new Object2D({
 					geometry: geometry,
 					position: { 
-						x: offsetX + i*config.bg.precision/window.innerWidth * 2 - 1, 
-						y: offsetY + j*config.bg.precision/window.innerHeight * 2 - 1
+						x: i*config.bg.precision/calcW * 2 - 1, 
+						y: j*config.bg.precision/calcH * 2 - 1
 					},
 					rotation: -Math.PI/2
 				}));
@@ -88,10 +94,11 @@ class Background {
 				[geometry[2].x, geometry[2].y]
 			);
 			bufferPosition.push( [x, y], [x, y], [x, y] );
+			
 			clientPosition.push(
-				[ (x+geometry[0].x +1)/2*window.innerWidth, (y+geometry[0].y+1)/2*window.innerHeight], 
-				[ (x+geometry[1].x+1)/2*window.innerWidth, (y+geometry[1].y+1)/2*window.innerHeight], 
-				[ (x+geometry[2].x+1)/2*window.innerWidth, (y+geometry[2].y+1)/2*window.innerHeight] 
+				[ (x+geometry[0].x+1)/2*calcW, (y+geometry[0].y+1)/2*calcH ], 
+				[ (x+geometry[1].x+1)/2*calcW, (y+geometry[1].y+1)/2*calcH ], 
+				[ (x+geometry[2].x+1)/2*calcW, (y+geometry[2].y+1)/2*calcH ] 
 			);
 
 		}
@@ -99,7 +106,9 @@ class Background {
 		this.meshInfo = {
 			position: bufferPosition,
 			localPosition: bufferLocalPosition,
-			sPosition: clientPosition
+			sPosition: clientPosition,
+			offsetX: offsetX,
+			offsetY: offsetY
 		}
 	
 	}
@@ -114,12 +123,16 @@ class Background {
 				}`,
 
 			vert: `
-				uniform float time;
 				uniform vec2 mouse;
 				
 				uniform vec2 waveCoords;
 				uniform float waveRadius;
 				uniform float waveStrength;
+	
+				uniform vec2 scroll; 
+				uniform vec2 offset;
+
+				uniform vec2 boundaries;
 
 				attribute vec2 position;
 				attribute vec2 localPosition;
@@ -127,22 +140,16 @@ class Background {
 				
 				void main() {
 					
-					float scaleFact = (1. + cos(time)) / 2.;
-					scaleFact = 1.;
+					vec2 newPosition = position + offset;
+
+					newPosition.y = mod(position.y + scroll.y, 2.) - 1.;
+
+
+					float intensityMouse = 1. - min(1., distance(mouse, newPosition)/0.2);
+					float intensityWave = waveStrength * 2. * (1. - min(1., abs(distance(waveCoords, newPosition) - waveRadius) / 0.3)) ;
 					
-					float intensityMouse = 1. - min(1., distance(mouse, sPosition)/200.);
-					float intensityWave = waveStrength * 2. * (1. - min(1., abs(distance(waveCoords, sPosition) - waveRadius) / 50.)) ;
-
-
-					vec2 newPosition = localPosition * (1. + intensityMouse*scaleFact + intensityMouse/2. + intensityWave);
-					
-
-
-					// newPosition.x = newPosition.x * cos(time) - newPosition.y * sin(time);
-					// newPosition.y = newPosition.y * cos(time) + newPosition.x * sin(time);
-					
-
-					newPosition += position;
+					newPosition += localPosition * (1. + intensityMouse*1.5 + intensityWave);
+					newPosition += localPosition;
 
 					gl_Position = vec4(newPosition, 0, 1);	
 				}
@@ -156,10 +163,12 @@ class Background {
 
 		  uniforms: {
 		  	time: () => { return this.time },
-		  	mouse: () => { return [this.cursorPosition.x, window.innerHeight - this.cursorPosition.y] },
+		  	mouse: () => { return this.cursorPositionScaled },
 		  	waveCoords: () => { return this.wave.coords },
 		  	waveRadius: () => { return this.wave.radius },
-		  	waveStrength: () => { return this.wave.strength }
+		  	waveStrength: () => { return this.wave.strength },
+		  	scroll: () => { return [0, this.scrollPercent] },
+		  	offset: () => { return [this.meshInfo.offsetX, this.meshInfo.offsetY] }
 		  },
 
 		  count: this.meshInfo.position.length
@@ -180,22 +189,30 @@ class Background {
 	}
 
 	onMouseMove(e) {
-		this.mouseCoord[0] = e.clientX;
-		this.mouseCoord[1] = window.innerHeight - e.clientY;
+		// this.mouseCoord[0] = e.clientX;
+		// this.mouseCoord[1] = window.innerHeight - e.clientY;
 
 		this.cursorExpectedPosition = {
 			x: e.clientX,
 			y: e.clientY
 		}
+
 		this.updateUntil(1500);
+	}
+
+	toShaderScale(coord){
+		return [
+			coord.x / window.innerWidth * 2 - 1,
+			coord.y / window.innerHeight * 2 - 1 
+		];
 	}
 
 	runWave(current){
 		if( current - this.wave.start > 700 ) {
 			this.wave = {
-				coords: [this.cursorPosition.x, window.innerHeight - this.cursorPosition.y],
+				coords: this.toShaderScale({ x: this.cursorPosition.x,  y: window.innerHeight - this.cursorPosition.y }),
 				radius: 0,
-				speed: 20,
+				speed: 0.05,
 				strength: 1,
 				needsUpdate: true,
 				start: current
@@ -227,6 +244,11 @@ class Background {
 				x: this.cursorPosition.x  + (this.cursorExpectedPosition.x - this.cursorPosition.x) * 0.1,
 				y: this.cursorPosition.y  + (this.cursorExpectedPosition.y - this.cursorPosition.y) * 0.1
 			}
+
+			this.cursorPositionScaled = this.toShaderScale({
+				x: this.cursorPosition.x,
+				y: window.innerHeight - this.cursorPosition.y
+			});
 
 			this.cursor.style = `transform: translate3d(${this.cursorPosition.x - 10}px, ${this.cursorPosition.y - 10}px, 0) scale(1)`
 		} 
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,13 +1,12 @@
 
 import Background from "./background";
 import BemElement from "./Bem.js";
-
-
-
+import Asap from "asap-js";
 
 
 
 window.addEventListener("load", function(){
 	var bg = new Background();
+	Asap.start();
 })
 
