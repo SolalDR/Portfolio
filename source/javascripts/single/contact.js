dee8b9f33265bf2d642adecada4c48ab3e9dfa78
SolalDR <solal.dussout-revel@hotmail.fr>
2018-02-18 17:15:11 +0100

    Start email management

--- source/javascripts/single/contact.js
+++ source/javascripts/single/contact.js
@@ -4,6 +4,22 @@ export default {
 	init: function(ctx){
 		this.ctx = ctx;
 		this.ctx.bg.clipCanvas.displayArrow("top");
-	}
 
+		var form = document.getElementById("sendmail");
+		form.addEventListener("submit", function(ev){
+			var fd = new FormData(this);
+			var oReq = new XMLHttpRequest();
+			oReq.open("POST", "/postmail.php", true);
+			
+			oReq.onload = function(oEvent) {
+				if (oReq.status == 200) {
+					console.log(oEvent)
+				} else {
+					console.log(oEvent)
+				}
+			};
+			oReq.send(fd);
+  			ev.preventDefault();
+		})
+	}
 }
